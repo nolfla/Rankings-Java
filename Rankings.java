351962e268a9568be92c914e57eadc5e56177c4c
nolfla <44060336+nolfla@users.noreply.github.com>
2022-01-12 09:52:59 -0700

    Create Rankings.java

---
+++ Rankings.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Rankings {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> kidsMovies = new ArrayList<String>();
+
+        kidsMovies.add("Moana");
+        kidsMovies.add("Up");
+        kidsMovies.add("WALL-E");
+
+        kidsMovies.set(0, "Babe");
+
+    }
+}
